3036141a4d2ae53fc18ec86ad19da92a81825fc4
EZ-7 <b.sh.tutor@gmail.com>
2024-06-09 20:44:00 +0000

    NodeJs kurs yakunlandi

--- 11.NPM/index.js
+++ 11.NPM/index.js
@@ -4,7 +4,7 @@ import http from 'http'
 const server = http.createServer((req, res) => {
 
     res.writeHead(202,'Good',{"Content-Type": 'text/html'})
-    res.write("<h1>Hello Node Js</h1>")
+    res.write("<h1>Hello Js</h1>")
 })
 // 8000 portga Serverni ishga tushirish
 server.listen(8000, ()=> console.log('server ishga tushdi'))
