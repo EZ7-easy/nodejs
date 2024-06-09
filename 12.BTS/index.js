167b4d40f77f10e5f0eccc154e55f1599bc7ddc3
EZ-7 <b.sh.tutor@gmail.com>
2024-06-09 20:31:25 +0000

    NodeJs darslari yakunlandi

---
+++ 12.BTS/index.js
@@ -0,0 +1,12 @@
+// Block code misol
+// console.log("Program Start")
+// alert('Block')
+// console.log("Program End")
+
+// Blocklanmagan code misol
+// console.log("Start")
+// setTimeout(()=>{
+//     alert('Bloklanmagan code')
+// },1000)
+
+// console.log('End')
